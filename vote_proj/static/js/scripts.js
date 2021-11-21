1c730e75872fccc588cce20321e10ecb4b60eabe
omanduj <omanduj@gmail.com>
2021-11-21 00:30:45 -0600

    Work around for images

--- vote_proj/static/js/scripts.js
+++ vote_proj/static/js/scripts.js
@@ -60,6 +60,30 @@ $("form[name=ballot_name]").submit(function(e){
         dataType: "json",
         success: function(resp) {
             console.log(resp);
+            window.location.href = "/ballot_items/"    //redirects on success to dashboard endpoint
+        },
+        error: function(resp) {
+            console.log(resp);
+            $error.text(resp.responseJSON.error).removeClass('error--hidden');       //if error, will show text that is found in return statement
+                                                    //of signup class. So it will print out the error whose key is error
+        }
+    })
+    e.preventDefault();
+});
+
+$("form[name=ballot_item]").submit(function(e){
+
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
+
+    $.ajax({
+        url: "/user/add_ballot_item", //this refers to url in flask layer to properly connect
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function(resp) {
+            console.log(resp);
                //redirects on success to dashboard endpoint
         },
         error: function(resp) {
